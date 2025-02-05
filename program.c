12fdb4c765e508116cb310c0c11619bf6bda345a
priyadharshini19-j <priyadharshini6505@gmail.com>
2025-02-05 14:10:41 +0530

    ver1

---
+++ program.c
@@ -0,0 +1,10 @@
+//This is my first program
+#include<stdio.h> //preprocessor directive/header file
+int main()
+{
+    char var_a;
+    printf("enter a character");
+    scanf("%c",&var_a);
+    printf("enter the character is %c",var_a);
+    return 0;
+}

27ab7cf0515adf4dbcb385151afe073652a40f25
priyadharshini19-j <priyadharshini6505@gmail.com>
2025-02-05 15:19:40 +0530

    ver4

---
+++ floatImp.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){
+    int num1,num2,num3;
+    float average;
+    num1=10.4;
+    num2=50;
+    num3=21.19;
+average=(num1+num2+num3)/3;
+printf("average =%f",average);
+   return 0;
+}

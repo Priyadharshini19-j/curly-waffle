c07939d61d5e3bc17c2dbcb280d35b223763f6b3
priyadharshini19-j <priyadharshini6505@gmail.com>
2025-02-05 15:24:04 +0530

    ver5

---
+++ floatExp.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main(){
+    int num1,num2,num3;
+    float average;
+    num1=104;
+    num2=50;
+    num3=21;
+average=(float)(num1+num2+num3)/3;
+printf("average =%f",average);
+   return 0;
+}
